8ecf63c8963ddb84162fb4fac8d239707a1bca63
kissshot404 <2261108841@qq.com>
2021-01-22 09:35:46 +0800

    添加简单的动态路由

--- project1/test.py
+++ project1/test.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+	return 'Hello World!'
+
+@app.route('/<int:pk>')
+def detail(pk):
+	return f'Hello World!{pk}'
+
+
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True,host='192.168.11.7',port='8888')
